c72d93afca361117d0a85f19668cafb64a7aa5f0
k07-repo <kpuli007@fiu.edu>
2020-10-10 13:01:37 -0400

    fixed cors import and added subject update endpoint

--- routes/subject.js
+++ routes/subject.js
@@ -8,7 +8,6 @@ const verifyToken = require("./verifyToken");
 const ObjectId = require("mongoose").Types.ObjectId;
 
 router.post("/", verifyToken, async (req, res) => {
-
   const { error, value } = subjectValidation(req.body);
   if (error) return res.status(400).send(error);
 
@@ -44,6 +43,26 @@ router.get("/:subjectId", async (req, res) => {
   return res.send(subject);
 });
 
+router.put("/:subjectId", async (req, res) => {
+  if (!ObjectId.isValid(req.params.subjectId))
+    return res.status(400).send({ message: "Invalid Id" });
+
+  const body = req.body
+
+  const subject = await Subject.findById(req.params.subjectId)
+
+  if(!subject) {
+    return res.status(400).send({ message: "No subject found for given ID" })
+  }
+
+  subject.title = body.title
+  subject.topics = body.topics
+  subject.questions = body.questions
+
+  const savedSubject = await subject.save()
+  return res.send(savedSubject)
+})
+
 router.delete("/:subjectId", verifyToken, async (req, res) => {
   if (!ObjectId.isValid(req.params.subjectId))
     return res.status(400).send({ message: "Invalid Id" });
@@ -51,5 +70,7 @@ router.delete("/:subjectId", verifyToken, async (req, res) => {
   await Topic.findByIdAndDelete(req.params.subjectId)
   return res.status(204).end()
 })
+
+
 
 module.exports = router;
