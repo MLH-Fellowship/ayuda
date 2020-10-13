fcc8a172b7e00dae808b4f1c6ad9e75688b27557
Laurell Seville <laurellseville2000@gmail.com>
2020-10-13 09:32:07 -0500

    working on reply to answers

--- ayuda-react-site/src/components/Answer.js
+++ ayuda-react-site/src/components/Answer.js
@@ -10,6 +10,7 @@ import ArrowDownwardOutlined from "@material-ui/icons/ArrowDownwardOutlined";
 import Card from "@material-ui/core/Card";
 import InfoCard from "../components/InfoCard";
 import { toTimestamp } from "../util/ToTimeStamp";
+import Link from "@material-ui/core/Link";
 
 
 
@@ -40,10 +41,24 @@ export default ({ id }) => {
             <ArrowDownwardOutlined />
           </IconButton>
         </div>
-        <div className="w-100">
-          <Typography align="left">{answer.text}</Typography>
 
-          <div className="d-flex justify-content-end">
+        <div className="w-100">
+          <Typography align="left" variant="body1">
+            {answer.text}
+          </Typography>
+
+          <div className="d-flex justify-content-between mt-2">
+            <Link
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            //   onClick={(e) => {
+            //     props.history.push(`/answer-question/${questionId}`);
+            //     preventDefault(e);
+            //   }}
+            >
+              <Typography style={{ cursor: "pointer" }}>
+                Reply to comment
+              </Typography>
+            </Link>
             <InfoCard
               text1={`Answered By ${answer.user.firstName} ${answer.user.lastName}`}
               text2={`            On ${postedAt.getDate()}/${postedAt.getMonth()}/
@@ -53,6 +68,31 @@ export default ({ id }) => {
           </div>
         </div>
       </div>
+      <hr />
+
+      {/* Shows Responses to the answer */}
+      <div className="d-flex">
+        <div style={{ width: "30%" }} />
+        <div className="d-flex flex-column mr-3">
+          <IconButton color="inherit">
+            <ArrowUpwardOutlined />
+          </IconButton>
+          24
+          <IconButton color="inherit">
+            <ArrowDownwardOutlined />
+          </IconButton>
+        </div>
+        <Typography align="left" variant="subtitle1">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+          venenatis vitae, justo.
+        </Typography>
+      </div>
+
       <hr />
     </div>
   );
