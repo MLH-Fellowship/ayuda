06362618e09eaaaa65f9790cf981130719395a2b
Laurell Seville <laurellseville2000@gmail.com>
2020-10-13 11:21:24 -0500

    updated ui to respond to replies to questions

--- ayuda-react-site/src/components/Answer.js
+++ ayuda-react-site/src/components/Answer.js
@@ -12,9 +12,7 @@ import InfoCard from "../components/InfoCard";
 import { toTimestamp } from "../util/ToTimeStamp";
 import Link from "@material-ui/core/Link";
 
-
-
-export default ({ id }) => {
+const Answer = ({ id }) => {
   const history = useHistory();
 
   const [answer, setAnswer] = useState();
@@ -30,7 +28,7 @@ export default ({ id }) => {
   let postedAt = new Date(toTimestamp(answer.createdAt));
 
   return (
-    <div>
+    <div className="w-100">
       <div className="d-flex align-items-center">
         <div className="d-flex flex-column mr-3">
           <IconButton color="inherit">
@@ -49,15 +47,15 @@ export default ({ id }) => {
 
           <div className="d-flex justify-content-between mt-2">
             <Link
-              style={{ cursor: "pointer" ,textDecoration: "none" }}
+              style={{ cursor: "pointer", textDecoration: "none" }}
               onClick={(e) => {
-                history.push(`/answer-question/${answer.question._id}/answers/${answer._id}`);
+                history.push(
+                  `/answer-question/${answer.question._id}/answers/${answer._id}`
+                );
                 e.preventDefault();
               }}
             >
-              <Typography>
-                Reply to comment
-              </Typography>
+              <Typography>Reply to comment</Typography>
             </Link>
             <InfoCard
               text1={`Answered By ${answer.user.firstName} ${answer.user.lastName}`}
@@ -71,29 +69,37 @@ export default ({ id }) => {
       <hr />
 
       {/* Shows Responses to the answer */}
-      <div className="d-flex">
-        <div style={{ width: "30%" }} />
-        <div className="d-flex flex-column mr-3">
-          <IconButton color="inherit">
-            <ArrowUpwardOutlined />
-          </IconButton>
-          24
-          <IconButton color="inherit">
-            <ArrowDownwardOutlined />
-          </IconButton>
-        </div>
-        <Typography align="left" variant="subtitle1">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
-          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-          venenatis vitae, justo.
-        </Typography>
-      </div>
+      {answer.replies.map((reply) => {
+        return (
 
-      <hr />
+            <div className="d-flex">
+              <div style={{ width: "10%" }} />
+              <Answer className="ml-2" id={reply._id} />
+            </div>
+
+          //   <div>
+          //     <div className="d-flex align-items-center">
+          //       <div style={{ width: "30%" }} />
+          //       <div className="d-flex flex-column mr-3">
+          //         <IconButton color="inherit">
+          //           <ArrowUpwardOutlined />
+          //         </IconButton>
+          //         24
+          //         <IconButton color="inherit">
+          //           <ArrowDownwardOutlined />
+          //         </IconButton>
+          //       </div>
+          //       <Typography align="left" variant="subtitle1">
+          //         {reply.text}
+          //       </Typography>
+          //     </div>
+
+          //     <hr />
+          //   </div>
+        );
+      })}
     </div>
   );
 };
+
+export default Answer;
